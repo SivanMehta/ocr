970a523b1b4164b71bfe7eb2e191c5827ff131ff
Sivan Mehta <skmehta@andrew.cmu.edu>
2021-04-01 13:09:55 -0400

    [wip] uploading file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function FileUpload() {
+  // State to store uploaded file
+  const [file, setFile] = useState(false);
+
+  return (
+    <div id="upload-box">
+      <input type="file" onChange={ e => setFile(e.target.files[0]) } />
+      <p>Filename: {file.name}</p>
+      <p>File type: {file.type}</p>
+      <p>File size: {file.size} bytes</p>
+      {file && <ImageThumb image={file} />}
+    </div>
+  );
+}
+
+/**
+ * Component to display thumbnail of image.
+ */
+const ImageThumb = ({ image }) => {
+  return <img src={URL.createObjectURL(image)} alt={image.name} />;
+};
 
 function App() {
-  return (
-    <h1>Hello World From React-Rollup</h1>
-  )
+  return <FileUpload />
 }
+
+
 export default App;
